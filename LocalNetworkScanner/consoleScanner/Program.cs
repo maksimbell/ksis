c7e5e0719178d6a8e075f3029656be0aea70a3df
Maksim Beliavsky <maksimbell503@gmail.com>
2022-03-04 13:40:20 +0300

    add error no interfaces event handler

--- LocalNetworkScanner/consoleScanner/Program.cs
+++ LocalNetworkScanner/consoleScanner/Program.cs
@@ -7,14 +7,22 @@ static void ScanInterfaces()
     Console.WriteLine("Device host name - {0}", deviceProps.HostName);
     Console.WriteLine("Interfaces count - {0}\n", adapters.Length);
 
-
-    foreach (NetworkInterface adapter in adapters)
+    if (adapters.Length != 0)
     {
-        /*Console.WriteLine(adapter.Name);*/
-        Console.WriteLine("Interface name: {0}", adapter.Description);
-        Console.WriteLine("Interface type: {0}", adapter.NetworkInterfaceType);
-        Console.WriteLine("Mac-address: {0}\n", adapter.GetPhysicalAddress());
+        foreach (NetworkInterface adapter in adapters)
+        {
+            /*Console.WriteLine(adapter.Name);*/
+            Console.WriteLine("Interface name: {0}", adapter.Description);
+            Console.WriteLine("Interface type: {0}", adapter.NetworkInterfaceType);
+            Console.WriteLine("Mac-address: {0}\n", BitConverter.ToString(adapter.GetPhysicalAddress().GetAddressBytes()));
+        }
     }
+    else
+    {
+        Console.WriteLine("No interfaces");
+    }
+
+    
 }
 
 ScanInterfaces();
