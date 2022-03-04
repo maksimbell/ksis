b8829db68216d8e33c32c1ae3d15b74566f81b9d
Maksim Beliavsky <maksimbell503@gmail.com>
2022-03-04 21:51:13 +0300

    export and use SendARP

--- LocalNetworkScanner/consoleScanner/Program.cs
+++ LocalNetworkScanner/consoleScanner/Program.cs
@@ -1,28 +1,90 @@
-﻿using System.Net.NetworkInformation;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
 
-static void ScanInterfaces()
+
+class Program
 {
-    IPGlobalProperties deviceProps = IPGlobalProperties.GetIPGlobalProperties();
-    NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
-    Console.WriteLine("Device host name - {0}", deviceProps.HostName);
-    Console.WriteLine("Interfaces count - {0}\n", adapters.Length);
-
-    if (adapters.Length != 0)
+    [DllImport("iphlpapi.dll", ExactSpelling = true)]
+    public static extern int SendARP(uint destinationIP, uint sourceIP,
+           byte[] macAddress, ref uint macAddressLength);
+    static void Main()
     {
-        foreach (NetworkInterface adapter in adapters)
+        Console.WriteLine("ss");
+        ScanLocalNetwork();
+    }
+
+    static NetworkInterface[] ScanInterfaces()
+    {
+        IPGlobalProperties deviceProps = IPGlobalProperties.GetIPGlobalProperties();
+        NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+        Console.WriteLine("Device host name - {0}", deviceProps.HostName);
+        Console.WriteLine("Interfaces count - {0}\n", adapters.Length);
+
+        return adapters;
+
+    }
+
+    static bool CheckWorkingInterface(NetworkInterface netInt)
+    {
+        return (netInt.NetworkInterfaceType != NetworkInterfaceType.Loopback &&
+                        netInt.OperationalStatus == OperationalStatus.Up);
+    }
+
+
+    static void ScanLocalNetwork()
+    {
+        NetworkInterface[] adapters = ScanInterfaces();
+
+        foreach (NetworkInterface net in adapters)
         {
-            /*Console.WriteLine(adapter.Name);*/
-            Console.WriteLine("Interface name: {0}", adapter.Description);
-            Console.WriteLine("Interface type: {0}", adapter.NetworkInterfaceType);
-            Console.WriteLine("Mac-address: {0}\n", BitConverter.ToString(adapter.GetPhysicalAddress().GetAddressBytes()));
+            ScanNetwork();
         }
     }
-    else
+
+    static void ScanNetwork(NetworkInterface netInterface)
     {
-        Console.WriteLine("No interfaces");
+        IPRangeResult ips = GetIPRange(netInterface);
+
+        List<uint> allIps = new List<uint>();
+        for (uint ip = ips.First; ip < ips.Last; IncrementIP(ref ip))
+        {
+            allIps.Add(ip);
+        }
+
+        Parallel.ForEach(allIps, ip =>
+        {
+            byte[] macAddress = new byte[6];
+            uint macAddressLength = 6;
+            int arpResult = SendARP(ip, ips.Local, macAddress, ref macAddressLength);
+
+            IPAddress add = new IPAddress(ip);
+            if (arpResult == 0)
+            {
+                Console.WriteLine($"{add} : {GetMacString(macAddress)}");
+            }
+        });
     }
 
-    
+    static string GetMacString(byte[] address)
+    {
+        StringBuilder macBuilder = new StringBuilder();
+        for (int i = 0; i < address.Length - 1; i++)
+        {
+            macBuilder.AppendFormat("{0:X2}-", address[i]);
+        }
+
+        macBuilder.AppendFormat("{0:X2}", address[^1]);
+
+        return macBuilder.ToString();
+    }
 }
 
-ScanInterfaces();
