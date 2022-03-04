a78dfc64431cecfc2188bc0d0cf7f217deb12ed4
Maksim Beliavsky <maksimbell503@gmail.com>
2022-03-04 13:13:34 +0300

    add local network scanning and info printing

---
+++ LocalNetworkScanner/consoleScanner/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.NetworkInformation;
+
+static void ScanInterfaces()
+{
+    IPGlobalProperties deviceProps = IPGlobalProperties.GetIPGlobalProperties();
+    NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+    Console.WriteLine("Device host name - {0}", deviceProps.HostName);
+    Console.WriteLine("Interfaces count - {0}\n", adapters.Length);
+
+
+    foreach (NetworkInterface adapter in adapters)
+    {
+        /*Console.WriteLine(adapter.Name);*/
+        Console.WriteLine("Interface name: {0}", adapter.Description);
+        Console.WriteLine("Interface type: {0}", adapter.NetworkInterfaceType);
+        Console.WriteLine("Mac-address: {0}\n", adapter.GetPhysicalAddress());
+    }
+}
+
+ScanInterfaces();
