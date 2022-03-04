47bab093f3015d0f128f1c0c6f6caf46342b5be7
Maksim Beliavsky <maksimbell503@gmail.com>
2022-03-05 00:46:44 +0300

    fix major bugs

--- LocalNetworkScanner/consoleScanner/Program.cs
+++ LocalNetworkScanner/consoleScanner/Program.cs
@@ -10,81 +10,164 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-
-class Program
+namespace consoleScanner
 {
-    [DllImport("iphlpapi.dll", ExactSpelling = true)]
-    public static extern int SendARP(uint destinationIP, uint sourceIP,
-           byte[] macAddress, ref uint macAddressLength);
-    static void Main()
+
+    readonly struct IPRangeResult
     {
-        Console.WriteLine("ss");
-        ScanLocalNetwork();
-    }
+        public uint Local { get; }
+        public uint First { get; }
+        public uint Last { get; }
 
-    static NetworkInterface[] ScanInterfaces()
-    {
-        IPGlobalProperties deviceProps = IPGlobalProperties.GetIPGlobalProperties();
-        NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
-        Console.WriteLine("Device host name - {0}", deviceProps.HostName);
-        Console.WriteLine("Interfaces count - {0}\n", adapters.Length);
-
-        return adapters;
-
-    }
-
-    static bool CheckWorkingInterface(NetworkInterface netInt)
-    {
-        return (netInt.NetworkInterfaceType != NetworkInterfaceType.Loopback &&
-                        netInt.OperationalStatus == OperationalStatus.Up);
-    }
-
-
-    static void ScanLocalNetwork()
-    {
-        NetworkInterface[] adapters = ScanInterfaces();
-
-        foreach (NetworkInterface net in adapters)
+        public IPRangeResult(uint local, uint first, uint last)
         {
-            ScanNetwork();
+            Local = local;
+            First = first;
+            Last = last;
         }
     }
 
-    static void ScanNetwork(NetworkInterface netInterface)
+    static class Program
     {
-        IPRangeResult ips = GetIPRange(netInterface);
-
-        List<uint> allIps = new List<uint>();
-        for (uint ip = ips.First; ip < ips.Last; IncrementIP(ref ip))
+        [DllImport("iphlpapi.dll", ExactSpelling = true)]
+        public static extern int SendARP(uint destinationIP, uint sourceIP,
+               byte[] macAddress, ref uint macAddressLength);
+        static void Main()
         {
-            allIps.Add(ip);
+            Console.WriteLine("!");
+            ScanLocalNetwork();
         }
 
-        Parallel.ForEach(allIps, ip =>
+        static NetworkInterface[] ScanInterfaces()
         {
-            byte[] macAddress = new byte[6];
-            uint macAddressLength = 6;
-            int arpResult = SendARP(ip, ips.Local, macAddress, ref macAddressLength);
+            IPGlobalProperties deviceProps = IPGlobalProperties.GetIPGlobalProperties();
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+            Console.WriteLine("Device host name - {0}", deviceProps.HostName);
+            Console.WriteLine("Interfaces count - {0}\n", adapters.Length);
 
-            IPAddress add = new IPAddress(ip);
-            if (arpResult == 0)
+            return adapters;
+
+        }
+
+        static bool CheckWorkingInterface(NetworkInterface netInt)
+        {
+            return (netInt.NetworkInterfaceType != NetworkInterfaceType.Loopback &&
+                            netInt.OperationalStatus == OperationalStatus.Up);
+        }
+
+
+        static void ScanLocalNetwork()
+        {
+            NetworkInterface[] adapters = ScanInterfaces();
+
+            foreach (NetworkInterface net in adapters)
             {
-                Console.WriteLine($"{add} : {GetMacString(macAddress)}");
+                if (CheckWorkingInterface(net))
+                {
+                    Console.WriteLine($"Interface - {net.Description}");
+                    ScanNetwork(net);
+                    Console.WriteLine("");
+                }
             }
-        });
-    }
-
-    static string GetMacString(byte[] address)
-    {
-        StringBuilder macBuilder = new StringBuilder();
-        for (int i = 0; i < address.Length - 1; i++)
-        {
-            macBuilder.AppendFormat("{0:X2}-", address[i]);
         }
 
-        macBuilder.AppendFormat("{0:X2}", address[^1]);
+        static void ScanNetwork(NetworkInterface netInterface)
+        {
+            IPRangeResult ips = GetIPRange(netInterface);
 
-        return macBuilder.ToString();
+            List<uint> allIps = new List<uint>();
+            for (uint ip = ips.First; ip < ips.Last; IncrementIP(ref ip))
+            {
+                allIps.Add(ip);
+            }
+
+            Console.WriteLine("_");
+
+            Parallel.ForEach(allIps, ip =>
+            {
+                byte[] macAddress = new byte[6];
+                uint macAddressLength = 6;
+                int arpResult = SendARP(ip, ips.Local, macAddress, ref macAddressLength);
+
+                IPAddress add = new IPAddress(ip);
+                if (arpResult == 0)
+                {
+                    Console.WriteLine($"{add} : {GetMacString(macAddress)}");
+                }
+            });
+        }
+
+        private static uint ToUInt(this byte[] bytes)
+        {
+            if (bytes.Length != 4)
+            {
+                throw new ArgumentException("Length of array is not 4");
+            }
+
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+
+
+            uint result = (uint)((bytes[0] << 24) | (bytes[1] << 16) | (bytes[2] << 8) | bytes[3]);
+            return result;
+        }
+
+        private static IPRangeResult GetIPRange(NetworkInterface netInterface)
+        {
+            foreach (UnicastIPAddressInformation ip in netInterface.GetIPProperties().UnicastAddresses)
+            {
+                if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    uint localIp = (ip.Address.GetAddressBytes()).ToUInt();
+                    uint subnet = (ip.IPv4Mask.GetAddressBytes()).ToUInt();
+
+                    uint first = localIp & subnet;
+                    uint last = first | (0xffffffff & ~subnet);
+
+                    if (BitConverter.IsLittleEndian)
+                    {
+                        first += 1 << 24;
+                    }
+                    else
+                    {
+                        first++;
+                        last--;
+                    }
+
+                    return new IPRangeResult(localIp, first, last);
+                }
+            }
+
+            throw new ArgumentException("Interface not scannable");
+        }
+
+        static string GetMacString(byte[] address)
+        {
+            StringBuilder macBuilder = new StringBuilder();
+            for (int i = 0; i < address.Length - 1; i++)
+            {
+                macBuilder.AppendFormat("{0:X2}-", address[i]);
+            }
+
+            macBuilder.AppendFormat("{0:X2}", address[^1]);
+
+            return macBuilder.ToString();
+        }
+
+        private static void IncrementIP(ref uint address)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                address += 1 << 24;
+            }
+            else
+            {
+                address++;
+            }
+        }
     }
+
 }
 
